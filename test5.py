7d3c984eac0132818571bc9e20f4affa81e7bd9e
SreeHarshaGudise <sreeharshadbz95@gmail.com>
2020-06-02 09:31:00 +0530

    Update test5.py
    Added code

--- test5.py
+++ test5.py
@@ -10,7 +10,7 @@ vendor_sales_case = trpt_vndr_case.\
 
 #Joins
 pre_df = vendor_sales_case.\
-    join(client_information,
+    join(client_Information,
          [(client_Information.CASE_RLSHP_TYP_CD == 'CNT') &
           (vendor_sales_case.CREW_OWNR_PO_ID == 1225977965)
           (vendor_sales_case.CREW_OWNR_PO_ID > 0) &
@@ -21,17 +21,23 @@ pre_df = vendor_sales_case.\
          'inner'
          )
 
-#columns
-bdg_atscase = pre_df.select(
-    col('VGI_Assigned_Case_ID'),
-    from_unixtime(unix_timestamp(pre_df.Case_Open_Date, 'yyyy-MM-dd')).alias('Case_Open_Date'),
-    when(from_unixtime(unix_timestamp(pre_df.EFFTV_END_TS, 'yyyy-MM-dd'))=='9999-12-31','null').
-        otherwise(from_unixtime(unix_timestamp(pre_df.EFFTV_END_TS, 'yyyy-MM-dd'))).alias('Case_Closed_Date'),
-    when(pre_df.CASE_STATUS_CD == 'OPEN','Open').when(pre_df.CASE_STATUS_CD=='CLSD','Closed').alias('Case_Status'),
-    when(pre_df.CASE_STATUS_CD == 'OPEN',datediff(date.today(),from_unixtime(unix_timestamp(pre_df.Case_Open_Date, 'yyyy-MM-dd'))))
-        .otherwise(datediff
-                       (when(from_unixtime(unix_timestamp(pre_df.EFFTV_END_TS, 'yyyy-MM-dd'))=='9999-12-31','null'),
-                        from_unixtime(unix_timestamp(pre_df.Case_Open_Date, 'yyyy-MM-dd'))
+
+#calculated columns
+final_df = pre_df\
+    .withColumn(when(col('CASE_STATUS_CD') == 'OPEN','Open').when(col('CASE_STATUS_CD') =='CLSD','Closed').alias('Case_Status'))\
+    .withColumn(when(col('CASE_STATUS_CD') == 'OPEN', datediff(to_date(lit(date.today())),from_unixtime(unix_timestamp(col('Case_Open_Date'), 'yyyy-MM-dd'))))
+                .otherwise(datediff
+                       (when(to_date(from_unixtime(unix_timestamp(col('EFFTV_END_TS'), 'yyyy-MM-dd'))) == '9999-12-31','null'),
+                        to_date(from_unixtime(unix_timestamp(col('Case_Open_Date'), 'yyyy-MM-dd')))
                         )
-                   ).alias('CycleTime'),
+                   ).alias('CycleTime'))
+
+#columns
+bdg_atscase = final_df.select(
+    col('VGI_Assigned_Case_ID'),
+    from_unixtime(unix_timestamp(final_df.Case_Open_Date, 'yyyy-MM-dd')).alias('Case_Open_Date'),
+    when(from_unixtime(unix_timestamp(final_df.EFFTV_END_TS, 'yyyy-MM-dd')) == '9999-12-31','null').
+        otherwise(from_unixtime(unix_timestamp(final_df.EFFTV_END_TS, 'yyyy-MM-dd'))).alias('Case_Closed_Date'),
+    col('Case_Status'),
+    col('CycleTime'),
     col('Owner_POID')).distinct()
