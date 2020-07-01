bd58e9ce82cb73043d1cb12abba55f963fa3fa26
SreeHarshaGudise <sreeharshadbz95@gmail.com>
2020-07-01 17:36:15 +0530

    Update test9.py
    Some validation Queries

--- test9.py
+++ test9.py
@@ -13,3 +13,69 @@ ats_counts = case_closed_df.\
     select(col('Owner_POID'),col('Case_Open_Date'),col('Case_Close_Date'),col('VGI_ASGND_CASE_ID'),col('Case_Status'))
 
 
+
+
+
+-----------------------------------------------------------------------------------------------------------------------
+VALIDATION QUERIES:
+    
+    
+ cognos table:
+    
+    SELECT COUNT(*)
+    ,POID
+    ,VGI_ASSGND_CASE_ID
+FROM profduction_table
+GROUP BY POID,VGI_ASSGND_CASE_ID
+order by POID,VGI_ASSGND_CASE_ID
+
+hive/aws table:
+    
+    
+    SELECT COUNT(*)
+    ,POID
+    ,VGI_ASSGND_CASE_ID
+FROM (your select statement logic)
+group by POID,VGI_ASSGND_CASE_ID
+order by POID,VGI_ASSGND_CASE_ID
+
+
+
+-------Check counts for a POID
+
+SELECT COUNT(*)
+    ,POID
+    ,VGI_ASSGND_CASE_ID
+FROM profduction_table
+WHERE POID IN (list_of_poids_you_need)
+GROUP BY POID,VGI_ASSGND_CASE_ID
+order by POID,VGI_ASSGND_CASE_ID
+
+
+
+ SELECT COUNT(*)
+    ,POID
+    ,VGI_ASSGND_CASE_ID
+FROM (your select statement logic) a
+WHERE a.POID in (list_of_poids_you_need)
+group by POID,VGI_ASSGND_CASE_ID
+order by POID,VGI_ASSGND_CASE_ID
+
+
+-------Check number of VGI_ASSGND_CASE_ID's for a POID
+
+
+SELECT COUNT(VGI_ASSGND_CASE_ID)
+    ,POID
+FROM profduction_table
+WHERE POID IN (list_of_poids_you_need)
+GROUP BY POID
+order by POID
+
+
+ SELECT COUNT(VGI_ASSGND_CASE_ID)
+    ,POID
+FROM (your select statement logic) a
+WHERE a.POID in (list_of_poids_you_need)
+group by POID
+order by POID
